35435cb849c9f95c3064386a1f1a4e9f0ba53b9b
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-05-20 23:41:46 -0700

    update db changes and markdown support

--- mao/admin.py
+++ mao/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
+from markdownx.admin import MarkdownxModelAdmin
+from markdownx.models import MarkdownxField
+from markdownx.widgets import AdminMarkdownxWidget
 from .models import rules
 
 class rules_admin(admin.ModelAdmin):
   verbose_name='Rules'
   plural_name='Rule'
+  formfield_overrides = {
+      MarkdownxField: {'widget': AdminMarkdownxWidget}
+  }
 
 
-admin.site.register(rules, rules_admin)
+admin.site.register(rules, MarkdownxModelAdmin)
--- mao/models.py
+++ mao/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from markdownx.models import MarkdownxField
 
 class rules(models.Model):
   name= models.CharField(max_length=60)
   activation= models.CharField(max_length=60)
-  rule_description= models.CharField(max_length=600)
+  rule_description= MarkdownxField()
   penalty= models.CharField(max_length=60)
   creator= models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
